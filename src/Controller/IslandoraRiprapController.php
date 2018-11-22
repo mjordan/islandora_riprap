e8b0b2f72c241d3ebfff7dd008d080033688393c
Mark Jordan <mjordan@sfu.ca>
2018-11-22 16:59:02 +0000

    Added some HTTP request stuff.

--- src/Controller/IslandoraRiprapController.php
+++ src/Controller/IslandoraRiprapController.php
@@ -10,8 +10,6 @@ use Drupal\Core\Access\AccessResult;
 * Controller.
 */
 class IslandoraRiprapController extends ControllerBase {
-  protected $representations;
-
   public function __construct() {
     $config = \Drupal::config('islandora_riprap.settings');
     $this->riprap_endpoint = $config->get('riprap_rest_endpoint') ?: 'http://localhost:8000/api/fixity';
@@ -32,23 +30,45 @@ class IslandoraRiprapController extends ControllerBase {
      ];
    }
 
-   /**
-    * Only show tab on nodes with the 'islandora_object' content type.
-    */
-   public function islandoraContentTypeOnly(NodeInterface $node = NULL) {
-     return ($node->getType() == 'islandora_object') ? AccessResult::allowed() : AccessResult::forbidden();
-   }
 
    /**
     * Get various representations of the object.
     */
    private function getRiprapEvents($nid) {
-/*
-     $url = 'http://localhost:8000/node/' . $nid . '?_format=' . $get_param;
-     $response = \Drupal::httpClient()->get($url);
-     $response_body = (string) $response->getBody();
-     $whole_object = json_decode($response_body, true);
-*/
+     try {
+       $container = \Drupal::getContainer();
+       $jwt = $container->get('jwt.authentication.jwt');
+       $auth = 'Bearer ' . $jwt->generateToken();
+       $client = \Drupal::httpClient();
+       $options = [
+         'auth' => [],
+         'headers' => ['Authorization' => $auth],
+         'form_params' => []
+       ];
+       $response = $client->request('GET', 'http://localhost:8000/node/' . $nid . '/media?_format=json', $options);
+       $code = $response->getStatusCode();
+       if ($code == 200) {
+         $body = $response->getBody()->getContents();
+       }
+       else {
+         \Drupal::logger('islandora_riprap')->error('HTTP response code: @code', array('@code' => $code));
+       }
+     }
+     catch (RequestException $e) {
+        \Drupal::logger('islandora_riprap')->error($e->getMessage());
+     }
+
+     /*
+     -Query curl -v -u admin:islandora http://localhost:8000/node/266/media?_format=json to get list of media.
+      @todo: How do we authenticate?
+     -For each media entry, perform the code at https://github.com/mjordan/islandora_riprap/issues/2.
+      This will get URLs for each media file.
+     -If necessary, convert each media file URL into its Fedora equivalent using Gemini. This will only be
+      necessary if Islandora is using Fedora (perhaps an admin option, or an autodetect if one is available).
+      The Riprap output will look like the same data below.
+     -For each media use (Preservation master, etc.), provide a summary of the total number of fixity checks,
+      plus number of success and failures.
+     */
 
      $sample_riprap_output = '[{"event_uuid":"cdecb9ac-5938-4992-b5b7-8ef9e4e94c62","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"fix","timestamp":"2018-10-20T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":"Fedora says hi."},{"event_uuid":"f26ed6cc-e6e2-4ebe-9607-5ad7a2e4b857","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"fix","timestamp":"2018-10-29T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":"Fedora says hi."},{"event_uuid":"af60c8d5-7504-4be0-a355-06177855b8b8","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"ing","timestamp":"2018-11-20T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"fail","event_outcome_detail_note":""}]';
      $riprap_output_as_array = json_decode($sample_riprap_output, true);
@@ -62,8 +82,30 @@ class IslandoraRiprapController extends ControllerBase {
          $failed_events++;
        }
      }
+
      $output = "Report from sample Riprap data (3 events): $successful_events successful events, $failed_events failed events.";
      return $output;
+   }
+
+   /**
+    * Only show tab on nodes with the 'islandora_object' content type.
+    */
+   public function islandoraContentTypeOnly(NodeInterface $node = NULL) {
+     return ($node->getType() == 'islandora_object') ? AccessResult::allowed() : AccessResult::forbidden();
+   }
+
+   /**
+    * Convert from term ID to term name.
+    *
+    * @param int $tid
+    *   The term ID.
+    *
+    * @return string
+    *   The term name.
+    */
+   public function tidToName($tid) {
+     $term = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->load($tid);
+     return $term->getName();
    }
 
 }
