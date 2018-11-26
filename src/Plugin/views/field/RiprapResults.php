d33a1e72d48d9c71db76069d5087666b8edd6ce8
Mark Jordan <mjordan@sfu.ca>
2018-11-26 04:57:15 +0000

    Work on adding Riprap results as a custom Views field.

--- src/Plugin/views/field/RiprapResults.php
+++ src/Plugin/views/field/RiprapResults.php
@@ -11,6 +11,13 @@ use Drupal\views\ResultRow;
  * @ViewsField("riprap_results")
  */
 class RiprapResults extends FieldPluginBase {
+  public function __construct() {
+    $config = \Drupal::config('islandora_riprap.settings');
+    $this->riprap_endpoint = $config->get('riprap_rest_endpoint') ?: 'http://localhost:8000/api/fixity';
+    $this->number_of_events = $config->get('number_of_events') ?: 10;
+    $this->use_drupal_urls = $config->get('use_drupal_urls') ?: FALSE;
+    $this->gemini_endpoint = $config->get('gemini_rest_endpoint') ?: 'http://localhost:8000/gemini';
+  }
 
   /**
    * @{inheritdoc}
@@ -22,8 +29,54 @@ class RiprapResults extends FieldPluginBase {
   /**
    * {@inheritdoc}
    */
-  public function render(ResultRow $values) {
-    // Render something more meaningful.
-    return ' Hi from Riprap!';
+  public function render(ResultRow $value) {
+    $media = $value->_entity;
+    $mid = $media->id();
+    $uuid = $media->uuid();
+    dd($mid . ':' . $this->gemini_endpoint . '/' . $uuid);
+    $url = $this->getFedoraUrl($uuid);
+    return $url;
   }
+
+   /**
+    * Get a Fedora URL for the entity from Gemini.
+    *
+    * @param string $uuid
+    *   The entity's UUID.
+    *
+    * @return string
+    *    The Fedora URL, or a message.
+    */
+  private function getFedoraUrl($uuid) {
+    try {
+      $container = \Drupal::getContainer();
+      $jwt = $container->get('jwt.authentication.jwt');
+      $auth = 'Bearer ' . $jwt->generateToken();
+      $client = \Drupal::httpClient();
+      $options = [
+        'http_errors' => false,
+        'headers' => ['Authorization' => $auth],
+      ];
+      $url = $this->gemini_endpoint . '/' . $uuid;
+      $response = $client->request('GET', $url, $options);
+      $code = $response->getStatusCode();
+      if ($code == 200) {
+        $body = $response->getBody()->getContents();
+        $body_array = json_decode($body, true);
+        return $body_array['fedora'];
+      }
+      elseif ($code == 404) {
+        $body = $response->getBody()->getContents();
+        return 'Not in Fedora';
+      }
+      else {
+        \Drupal::logger('islandora_riprap')->error('HTTP response code: @code', array('@code' => $code));
+      }
+    }
+    catch (RequestException $e) {
+       \Drupal::logger('islandora_riprap')->error($e->getMessage());
+       return "Sorry, there has been an error, please refer to the system log";
+    }
+  }
+
 }
