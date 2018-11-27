9dfece0f3555947d5810bd6a75413c9a28ba5a44
Mark Jordan <mjordan@sfu.ca>
2018-11-27 00:52:41 +0000

    Work on adding Riprap results as a custom Views field.

--- README.md
+++ README.md
@@ -6,11 +6,7 @@ Beginnings of a Drupal 8 module to provide node-level reports using data from th
 
 ## Overview
 
-Currently under development. So far, adds a "Fixity auditing" menu item to each node of `islandora_object` content type and displays an list of Fedora URLs for Media associated with the node:
-
-![details](docs/islandora_riprap_details.png)
-
-The goal is to replace the raw URLs with a report indicating how many successful and failed Riprap fixity events for each Media file.
+Currently under development. So far, adds a "Fixity auditing" field to the "Media" tab (which is actually the output of the "Manage Media" view provided by the Islandora module) showing Fedora URLs for Media associated with the node. The goal is to replace the raw URLs with a report indicating how many successful and failed Riprap fixity events for each Media file.
 
 ## Requirements
 
@@ -21,8 +17,20 @@ The goal is to replace the raw URLs with a report indicating how many successful
 
 1. Clone this repo into your Islandora's `drupal/web/modules/contrib` directory.
 1. Enable the module either under the "Admin > Extend" menu or by running `drush en -y islandora_riprap`.
+1. Add the "Fixity Auditing" field to the "Manage Media" View:
+  1. In your list of Views, click on the "Edit" button for the "Manage Media" View.
+  1. In the "Page" display, click on the "Add" Fields button.
+  1. From the list of fields, check "Fixity Auditing."
+  1. Click on "Apply (this display)".
+  1. Change the label if you want.
+  1. Click on "Apply (this display)".
+  1. Click on the "Save" button to save the change to the View.
 
-## Installation
+Now, when you click on the "Media" tab in an Islandora object node, you will see a new column in the table showing (for now) the Fedora URL for the media file:
+
+![details](docs/islandora_riprap_details.png)
+
+## Configuration
 
 1. Go to Drupal's "Configuration" menu.
 1. In the "Islandora" section, click on the "Fixity auditing" link.
--- src/Plugin/views/field/RiprapResults.php
+++ src/Plugin/views/field/RiprapResults.php
@@ -8,17 +8,11 @@ use Drupal\views\ResultRow;
 /**
  * Field plugin that renders data for Media from Riprap.
  *
+ * @ingroup views_field_handlers
+ *
  * @ViewsField("riprap_results")
  */
 class RiprapResults extends FieldPluginBase {
-  public function __construct() {
-    $config = \Drupal::config('islandora_riprap.settings');
-    $this->riprap_endpoint = $config->get('riprap_rest_endpoint') ?: 'http://localhost:8000/api/fixity';
-    $this->number_of_events = $config->get('number_of_events') ?: 10;
-    $this->use_drupal_urls = $config->get('use_drupal_urls') ?: FALSE;
-    $this->gemini_endpoint = $config->get('gemini_rest_endpoint') ?: 'http://localhost:8000/gemini';
-  }
-
   /**
    * @{inheritdoc}
    */
@@ -30,23 +24,59 @@ class RiprapResults extends FieldPluginBase {
    * {@inheritdoc}
    */
   public function render(ResultRow $value) {
+    $config = \Drupal::config('islandora_riprap.settings');
+    $this->riprap_endpoint = $config->get('riprap_rest_endpoint') ?: 'http://localhost:8000/api/fixity';
+    $this->number_of_events = $config->get('number_of_events') ?: 10;
+    $this->use_drupal_urls = $config->get('use_drupal_urls') ?: FALSE;
+    $this->gemini_endpoint = $config->get('gemini_rest_endpoint') ?: 'http://localhost:8000/gemini';
+
     $media = $value->_entity;
     $mid = $media->id();
-    $uuid = $media->uuid();
-    dd($mid . ':' . $this->gemini_endpoint . '/' . $uuid);
-    $url = $this->getFedoraUrl($uuid);
+    $binary_resource_uuid = $this->getFileUuid($mid);
+    $url = $this->getFedoraUrl($binary_resource_uuid);
     return $url;
   }
 
-   /**
-    * Get a Fedora URL for the entity from Gemini.
-    *
-    * @param string $uuid
-    *   The entity's UUID.
-    *
-    * @return string
-    *    The Fedora URL, or a message.
-    */
+  /**
+   * Given a Media id, get the corresponding FileUUID.
+   *
+   * @param int $mid
+   *   A Media ID.
+   *
+   * @return string
+   *   The UUID of the file associated with the incoming Media entity.
+   */
+  public function getFileUuid($mid) {
+    $media_fields = array(
+      'field_media_file',
+      'field_media_image',
+      'field_media_audio_file',
+      'field_media_video_file',
+    );
+    $media = \Drupal\Media\Entity\Media::load($mid);
+    // Loop through each of the media fields and get the UUID of the File
+    // in the first one encountered. Assumes each Media entity has only
+    // one of the media file fields.
+    foreach ($media_fields as $media_field) {
+      if (isset($media->$media_field)) {
+        $files = $media->get($media_field);
+        $file = $files->first();
+        $target_file = $file->get('entity')->getTarget();
+        $target_file_uuid_array = $target_file->get('uuid')->getValue();
+        return $target_file_uuid_array[0]['value'];
+      }
+    }
+  }
+
+  /**
+   * Get a Fedora URL for the entity from Gemini.
+   *
+   * @param string $uuid
+   *   The entity's UUID.
+   *
+   * @return string
+   *    The Fedora URL, or a message.
+  */
   private function getFedoraUrl($uuid) {
     try {
       $container = \Drupal::getContainer();
