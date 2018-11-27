2154dbb2b4ea02c7e3a55f38b11961e7fb914011
Mark Jordan <mjordan@sfu.ca>
2018-11-27 22:56:50 +0000

    Put sample Riprap data in a table.

--- src/Controller/IslandoraRiprapController.php
+++ src/Controller/IslandoraRiprapController.php
@@ -26,6 +26,20 @@ class IslandoraRiprapController extends ControllerBase {
      $path_args = explode('/', $current_path);
      $mid = $path_args[2];
      $output = $this->queryRiprap($mid);
+
+     $header = ['Event UUID', 'Event type', 'Timestamp', 'Digest algorithm', 'Digest value', 'Event detail', 'Event outcome', 'Note'];
+     $rows = array();
+     foreach ($output as &$event) {
+       unset($event['resource_id']);
+       $rows[] = array_values($event);
+     }
+
+     $output = [
+       '#theme' => 'table',
+       '#header' => $header,
+       '#rows' => $rows,
+     ];
+
      return [
        '#theme' => 'islandora_riprap_report',
        '#report' => $output,
@@ -42,8 +56,9 @@ class IslandoraRiprapController extends ControllerBase {
    *   The JSON response from Riprap.
    */
   private function queryRiprap($mid) {
-    $sample_riprap_output = '[{"event_uuid":"cdecb9ac-5938-4992-b5b7-8ef9e4e94c62","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"fix","timestamp":"2018-10-20T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":"Fedora says hi."},{"event_uuid":"f26ed6cc-e6e2-4ebe-9607-5ad7a2e4b857","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"fix","timestamp":"2018-10-29T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":"Fedora says hi."},{"event_uuid":"af60c8d5-7504-4be0-a355-06177855b8b8","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"ing","timestamp":"2018-11-20T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"fail","event_outcome_detail_note":""}]';
-    return $sample_riprap_output;
+    $sample_riprap_output = '[{"event_uuid":"cdecb9ac-5938-4992-b5b7-8ef9e4e94c62","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"fix","timestamp":"2018-10-20T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":"Fedora says hi."},{"event_uuid":"f26ed6cc-e6e2-4ebe-9607-5ad7a2e4b857","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"fix","timestamp":"2018-10-29T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":"Fedora says hi."},{"event_uuid":"af60c8d5-7504-4be0-a355-06177855b8b8","resource_id":"http:\/\/localhost:8000\/mockrepository\/rest\/11","event_type":"ing","timestamp":"2018-11-20T07:35:04-0800","digest_algorithm":"SHA-1","digest_value":"339e2ebc99d2a81e7786a466b5cbb9f8b3b81377","event_detail":"","event_outcome":"suc","event_outcome_detail_note":""}]';
+    $sample_riprap_output_array = json_decode($sample_riprap_output, true);
+    return $sample_riprap_output_array;
   }
 
 }
