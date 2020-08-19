f713b2558157c9130d680b8c6eb3605ebba26dd8
Mark Jordan <mjordan@sfu.ca>
2020-08-19 03:00:35 +0000

    Address #33.

---
+++ src/Controller/IslandoraRiprapGetHashController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\islandora_riprap\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+/**
+ * Controller.
+ */
+class IslandoraRiprapGetHashController extends ControllerBase {
+
+  /**
+   * Gets the hash of the file identified by the UUID.
+   *
+   * @param string $file_uuid
+   *   The UUID of the file.
+   * @param string $algorith
+   *   One of 'md5', 'sha1', or 'sha256'.
+   *
+   * @return JsonResponse
+   *   A JSON response. 
+   */
+  public function main($file_uuid, $algorithm) {
+    $file = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uuid' => $file_uuid]);
+    $file = reset($file);    
+    $checksum = hash_file($algorithm, $file->getFileUri());
+    $response[] = [
+      'checksume' => $checksum,
+      'file_uuid' => $file_uuid,
+      'algorithm' => $algorithm,
+    ];
+
+    return new JsonResponse($response);
+  }
+
+}
